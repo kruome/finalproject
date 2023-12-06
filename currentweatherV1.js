7eb4927cef0955ee892f677a1f5bc78ec644cd01
kruome <71830435+kruome@users.noreply.github.com>
2023-12-05 22:38:54 -0500

    Create currentweatherV1.js
    Initial Version of the code

---
+++ currentweatherV1.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./current-weather.css";
+
+const CurrentWeather = ({ data }) => {
+  return (
+    <div className="weather">
+    </div>
+  );
+};
+
+export default CurrentWeather;
