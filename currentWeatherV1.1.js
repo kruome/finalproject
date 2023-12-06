00b2f76fde1fb7b331234accfa424cd14f77e0ca
tnguyen7301 <152452414+tnguyen7301@users.noreply.github.com>
2023-12-05 22:46:11 -0500

    Create currentWeatherV1.1.js
    Second update to currentWeather function

---
+++ currentWeatherV1.1.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./current-weather.css";
+
+const CurrentWeather = ({ data }) => {
+  return (
+    <div className="weather">
+      <div className="top">
+        <div>
+          <p className="city">{data.city}</p>
+          <p className="weather-description">{data.weather[0].description}</p>
+        </div>
+        <img
+          alt="weather"
+          className="weather-icon"
+          src={`icons/${data.weather[0].icon}.png`}
+        />
+      </div>
+      <div className="bottom">
+        <p className="temperature">{Math.round(data.main.temp)}°C</p>
+        <div className="details">
+          <div className="parameter-row">
+            <span className="parameter-label">Details</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CurrentWeather;
